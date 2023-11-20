1b0f2e9a28e537abfce36b05bcbbe0e9d3fdcaf0
Rahul Chaurasia <rahulchaurasia966@gmail.com>
2023-11-20 22:19:37 +0530

    client ui updated

--- client/src/features/client/Details.jsx
+++ client/src/features/client/Details.jsx
@@ -13,21 +13,31 @@ export const Details = () => {
 
   useEffect(() => {
     dispatch(detailsOfClientsThunk(id));
+
   }, []);
+  console.log(client,"client setails ");
 
   return (
     <Container maxWidth="sm" className="text-center">
       <div className="max-w-screen-sm mx-auto my-4 p-4 bg-white shadow-lg rounded-lg text-left">
-        <Typography variant="h4" component="h2" gutterBottom underline="true">
-          Details Of Client
-        </Typography>
+      <Typography variant="h4" component="h2" align="center" gutterBottom style={{ textDecoration: 'underline' }}>
+  Details Of Client
+</Typography>
+
 
         <div className="text-left">
           <p className="mb-2">
             <strong>Name:</strong> {client?.firstName} {client?.lastName}
           </p>
           <p className="mb-2">
-            <strong>Primary Phone Number:</strong> {client?.primaryPhoneNumber}
+            <strong>Email:</strong> {client?.email} 
+          </p>
+          
+          <p className="mb-2">
+            <strong>Gender:</strong> {client?.gender}
+          </p> 
+          <p className="mb-2">
+            <strong>Primary phone No.:</strong> {client?.primaryPhoneNumber}
           </p>
           <p className="mb-2">
             <strong>Address:</strong> {client?.address?.address1}
@@ -44,9 +54,8 @@ export const Details = () => {
           <p className="mb-2">
             <strong>Date Of Birth:</strong> {client?.dob}
           </p>
-          <p className="mb-2">
-            <strong>Default Location:</strong> {client?.location?.name}
-          </p>
+     
+        
         </div>
       </div>
     </Container>
--- client/src/features/client/List.jsx
+++ client/src/features/client/List.jsx
@@ -27,7 +27,7 @@ export const List = () => {
       <Typography variant="h4" component="h2" align="center" sx={{ textDecoration: 'underline' }}>
         Clients List
       </Typography>
-      <section className="flex justify-between items-center mb-4">
+      <section className="flex justify-left items-center mb-4">
         <Button
           component={Link}
           to={"create"}
@@ -37,52 +37,51 @@ export const List = () => {
           Create Client
         </Button>
       </section>
-      <Box sx={{ overflowX: "auto" }}>
-        <TableContainer component={Paper}>
-          <Table aria-label="Clients Table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Serial No.</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Actions</TableCell>
+      <Box sx={{ overflowX: "auto", display: 'flex', justifyContent: 'center' }}>
+        <Table aria-label="Clients Table">
+          <TableHead>
+            <TableRow>
+              <TableCell>S.No.</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {list.map((item, index) => (
+              <TableRow key={item?._id}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{item?.firstName + " " + item?.lastName}</TableCell>
+                <TableCell>
+                  <IconButton
+                    component={Link}
+                    to={`details/${item?._id}`}
+                    color="primary"
+                  >
+                    <Visibility />
+                  </IconButton>
+                  <IconButton
+                    component={Link}
+                    to={`edit/${item?._id}`}
+                    color="warning"
+                  >
+                    <Edit />
+                  </IconButton>
+                  <IconButton
+                    onClick={() => handleDelete(item?._id)}
+                    color="error"
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {list.map((item, index) => (
-                <TableRow key={item?._id}>
-                  <TableCell>{index + 1}</TableCell>
-                  <TableCell>{item?.firstName + " " + item?.lastName}</TableCell>
-                  <TableCell>
-                    <IconButton
-                      component={Link}
-                      to={`details/${item?._id}`}
-                      color="primary"
-                    >
-                      <Visibility />
-                    </IconButton>
-                    <IconButton
-                      component={Link}
-                      to={`edit/${item?._id}`}
-                      color="warning"
-                    >
-                      <Edit />
-                    </IconButton>
-                    <IconButton
-                      onClick={() => handleDelete(item?._id)}
-                      color="error"
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+            ))}
+          </TableBody>
+        </Table>
       </Box>
     </div>
   );
 };
+
 
 // import React, { useEffect } from "react";
 // import { Link } from "react-router-dom";
