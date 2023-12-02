3a271ead02f632096aa39f45c11c15c9810a703a
Rahul Chaurasia <rahulchaurasia966@gmail.com>
2023-12-02 11:24:45 +0530

    clinician list and Create component is updated

--- client/src/features/clinician/Create.jsx
+++ client/src/features/clinician/Create.jsx
@@ -71,171 +71,376 @@ export const Create = () => {
       toast.error("An error occurred while adding Clinician: " + error.message);
     }
   };
-
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          minHeight: "100vh",
-        }}
-      >
-        <div className="card card-container bg-white p-6 rounded-md shadow-md">
-          <Typography
-            variant="h4"
-            component="h2"
-            align="center"
-            gutterBottom
-            sx={{ textDecoration: "underline" }}
-          >
-            New Clinician Details
-          </Typography>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={handleCreatingClinician}
-          >
-            <Form>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    label="First Name"
-                    name="firstName"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Last Name"
-                    name="lastName"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Primary Phone No."
-                    name="primaryPhoneNumber"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Address1"
-                    name="address.address1"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="City"
-                    name="address.city"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="State"
-                    name="address.state"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="ZipCode"
-                    name="address.zipCode"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Bio Link"
-                    name="bioLink"
-                    type="text"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Email"
-                    name="person.email"
-                    type="email"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Password"
-                    name="person.secret"
-                    type="password"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    label="Confirm Password"
-                    name="passowrd"
-                    type="password"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                  />
-                </Grid>
-                <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    size="small"
-                  >
-                    Submit
-                  </Button>
-                </Grid>
-              </Grid>
-            </Form>
-          </Formik>
-        </div>
-      </Box>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-    </Container>
+    <div className="col-md-12 login-form">
+      <div className="card card-container bg-white p-6 rounded-md shadow-md">
+        <h2 className="text-2xl font-semibold mb-4">New Clinician Details</h2>
+        <Formik
+         initialValues={initialValues}
+                   validationSchema={validationSchema}
+                  onSubmit={handleCreatingClinician}
+        >
+          <Form>
+            <div className="form-group">
+              <label
+                htmlFor="Name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                First Name
+              </label>
+              
+              <Field
+                name="firstName"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="last"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Last Name
+              </label>
+              <Field
+                name="lastName"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <Field
+                name="email"
+                type="email"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="last"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Gender
+              </label>
+              <Field
+                name="gender"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+
+            <div className="form-group">
+              <label
+                htmlFor="last"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Primary Phone No.
+              </label>
+              <Field
+                name="primaryPhoneNumber"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="last"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Address1
+              </label>
+           
+              <Field
+                name="address.address1"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="last"
+                className="block text-sm font-medium text-gray-700"
+              >
+                City
+              </label>
+              
+              <Field
+                name="address.city"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="last"
+                className="block text-sm font-medium text-gray-700"
+              >
+                State
+              </label>
+              <Field
+                name="address.state"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="last"
+                className="block text-sm font-medium text-gray-700"
+              >
+                ZipCode
+              </label>
+              <Field
+                name="address.zipCode"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="bioLink"
+                className="block text-sm font-medium text-gray-700"
+              >
+               Bio Link
+              </label>
+           
+ 
+              <Field
+                name="bioLink"
+                type="text"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="Email"
+                className="block text-sm font-medium text-gray-700"
+              >
+               Email
+              </label>
+          
+
+ 
+              <Field
+                name="person.email"
+                type="email"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="Password"
+                className="block text-sm font-medium text-gray-700"
+              >
+               Password
+              </label>
+             
+ 
+              <Field
+                name="person.secret"
+                type="password"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div className="form-group">
+              <label
+                htmlFor="passowrd"
+                className="block text-sm font-medium text-gray-700"
+              >
+               Confirm Password
+              </label>
+           
+             
+              <Field
+                name="passowrd"
+                type="passowrd"
+                className="block w-full mt-1 p-2 border rounded-md focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+
+            <div className="form-group">
+              <button
+                type="submit"
+                className="w-full mt-4 p-2 bg-indigo-500 text-white font-semibold rounded-md hover:bg-indigo-700"
+              >
+                Submit
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
   );
+  // return (
+  //   <Container maxWidth="lg">
+  //     <Box
+  //       sx={{
+  //         display: "flex",
+  //         flexDirection: "column",
+  //         alignItems: "center",
+  //         justifyContent: "center",
+  //         minHeight: "100vh",
+  //       }}
+  //     >
+  //       <div className="card card-container bg-white p-6 rounded-md shadow-md">
+  //         <Typography
+  //           variant="h4"
+  //           component="h2"
+  //           align="center"
+  //           gutterBottom
+  //           sx={{ textDecoration: "underline" }}
+  //         >
+  //           New Clinician Details
+  //         </Typography>
+  //         <Formik
+  //           initialValues={initialValues}
+  //           validationSchema={validationSchema}
+  //           onSubmit={handleCreatingClinician}
+  //         >
+  //           <Form>
+  //             <Grid container spacing={2}>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="First Name"
+  //                   name="firstName"
+  //                   type="text"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="Last Name"
+  //                   name="lastName"
+  //                   type="text"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="Primary Phone No."
+  //                   name="primaryPhoneNumber"
+  //                   type="text"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="Address1"
+  //                   name="address.address1"
+  //                   type="text"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="City"
+  //                   name="address.city"
+  //                   type="text"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="State"
+  //                   name="address.state"
+  //                   type="text"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="ZipCode"
+  //                   name="address.zipCode"
+  //                   type="text"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="Bio Link"
+  //                   name="bioLink"
+  //                   type="text"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="Email"
+  //                   name="person.email"
+  //                   type="email"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="Password"
+  //                   name="person.secret"
+  //                   type="password"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12}>
+  //                 <TextField
+  //                   label="Confirm Password"
+  //                   name="passowrd"
+  //                   type="password"
+  //                   variant="outlined"
+  //                   margin="normal"
+  //                   fullWidth
+  //                 />
+  //               </Grid>
+  //               <Grid item xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+  //                 <Button
+  //                   type="submit"
+  //                   variant="contained"
+  //                   color="primary"
+  //                   size="small"
+  //                 >
+  //                   Submit
+  //                 </Button>
+  //               </Grid>
+  //             </Grid>
+  //           </Form>
+  //         </Formik>
+  //       </div>
+  //     </Box>
+  //     <ToastContainer
+  //       position="top-right"
+  //       autoClose={3000}
+  //       hideProgressBar={false}
+  //       newestOnTop={false}
+  //       closeOnClick
+  //       rtl={false}
+  //       pauseOnFocusLoss
+  //       draggable
+  //       pauseOnHover
+  //     />
+  //   </Container>
+  // );
 };
--- client/src/features/clinician/List.jsx
+++ client/src/features/clinician/List.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from "@mui/material";
+
 import { listOfClinicianThunk } from "./listSlice";
-import IconButton from "@mui/material/IconButton";
-import axios from "axios";
+import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, IconButton, Box } from "@mui/material";
 import { Delete, Edit, Visibility } from "@mui/icons-material";
+import DeleteIcon from "@mui/icons-material/Delete";
+// import IconButton from "@mui/material/IconButton";
+import axios from "axios";
+// import { Delete, Edit, Visibility } from "@mui/icons-material";
 
 export const List = () => {
   const dispatch = useDispatch();
@@ -22,76 +25,134 @@ export const List = () => {
       .delete(`https://clinic-connect-backend.onrender.com/api/v1/clinicians/deleteclinician/${id}`)
       .then(() => dispatch(listOfClinicianThunk()));
   };
-
   return (
     <div className="container mx-auto mt-6">
-<Typography variant="h4" component="h2" align="center" sx={{ textDecoration: 'underline' }}>
+      <Typography variant="h4" component="h2" align="center" sx={{ textDecoration: 'underline' }}>
         Clinicians List
       </Typography>
-      <section className="flex justify-between items-center mb-4">
-       
+      <section className="flex justify-left items-center mb-4">
         <Button
-                    component={Link}
-                    to={"create"}
-                    variant="contained"
-                    color="primary"
-                 
-                  >
-                       Create Clinician
-                  </Button>
+          component={Link}
+          to={"create"}
+          variant="contained"
+          color="primary"
+        >
+          Create Clinician
+        </Button>
       </section>
-     
-      <TableContainer component={Paper} className="mt-4">
+      <Box sx={{ overflowX: "auto", display: 'flex', justifyContent: 'center' }}>
         <Table aria-label="Clients Table">
           <TableHead>
             <TableRow>
+              <TableCell>S.No.</TableCell>
               <TableCell>Name</TableCell>
-              <TableCell>Read</TableCell>
-              <TableCell>Update</TableCell>
-              <TableCell>Delete</TableCell>
+              <TableCell>Actions</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {list.map((item) => (
+            {list.map((item, index) => (
               <TableRow key={item?._id}>
+                <TableCell>{index + 1}</TableCell>
                 <TableCell>{item?.firstName + " " + item?.lastName}</TableCell>
                 <TableCell>
-              
-                  <IconButton 
-                   component={Link}
-                     to={`details/${item?._id}`}
-                  color="primary"
+                  <IconButton
+                    component={Link}
+                    to={`details/${item?._id}`}
+                    color="primary"
                   >
-        <Visibility />
-      </IconButton>
-                </TableCell>
-                <TableCell>
-              
-                  <IconButton 
-                   component={Link}
-                   to={`edit/${item?._id}`}
-                  color="warning"
+                    <Visibility />
+                  </IconButton>
+                  <IconButton
+                    component={Link}
+                    to={`edit/${item?._id}`}
+                    color="warning"
                   >
-        <Edit />
-      </IconButton>
-                </TableCell>
-                <TableCell>
-                 
-                  <IconButton 
-                   onClick={() => handleDelete(item?._id)}
-              
-               
-                  color="error"
+                    <Edit />
+                  </IconButton>
+                  <IconButton
+                    onClick={() => handleDelete(item?._id)}
+                    color="error"
                   >
-        <Delete />
-      </IconButton>
-                  
+                    <DeleteIcon />
+                  </IconButton>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
-      </TableContainer>
+      </Box>
     </div>
   );
+
+//   return (
+//     <div className="container mx-auto mt-6">
+// <Typography variant="h4" component="h2" align="center" sx={{ textDecoration: 'underline' }}>
+//         Clinicians List
+//       </Typography>
+//       <section className="flex justify-between items-center mb-4">
+       
+//         <Button
+//                     component={Link}
+//                     to={"create"}
+//                     variant="contained"
+//                     color="primary"
+                 
+//                   >
+//                        Create Clinician
+//                   </Button>
+//       </section>
+     
+//       <TableContainer component={Paper} className="mt-4">
+//         <Table aria-label="Clients Table">
+//           <TableHead>
+//             <TableRow>
+//               <TableCell>Name</TableCell>
+//               <TableCell>Read</TableCell>
+//               <TableCell>Update</TableCell>
+//               <TableCell>Delete</TableCell>
+//             </TableRow>
+//           </TableHead>
+//           <TableBody>
+//             {list.map((item) => (
+//               <TableRow key={item?._id}>
+//                 <TableCell>{item?.firstName + " " + item?.lastName}</TableCell>
+//                 <TableCell>
+              
+//                   <IconButton 
+//                    component={Link}
+//                      to={`details/${item?._id}`}
+//                   color="primary"
+//                   >
+//         <Visibility />
+//       </IconButton>
+//                 </TableCell>
+//                 <TableCell>
+              
+//                   <IconButton 
+//                    component={Link}
+//                    to={`edit/${item?._id}`}
+//                   color="warning"
+//                   >
+//         <Edit />
+//       </IconButton>
+//                 </TableCell>
+//                 <TableCell>
+                 
+//                   <IconButton 
+//                    onClick={() => handleDelete(item?._id)}
+              
+               
+//                   color="error"
+//                   >
+//         <Delete />
+//       </IconButton>
+                  
+//                 </TableCell>
+//               </TableRow>
+//             ))}
+//           </TableBody>
+//         </Table>
+//       </TableContainer>
+//     </div>
+//   );
 };
