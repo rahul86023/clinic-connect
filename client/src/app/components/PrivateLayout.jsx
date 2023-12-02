0b4a3bb083051129e6fe689145772cf7f3574663
Rahul Chaurasia <rahulchaurasia966@gmail.com>
2023-12-02 16:56:23 +0530

    build created

--- client/src/app/components/PrivateLayout.jsx
+++ client/src/app/components/PrivateLayout.jsx
@@ -21,20 +21,3 @@ export const PrivateLayout = () => {
   );
 };
 
-// import { Navigate, Outlet } from "react-router-dom";
-// import Navbar from "./Navbar";
-// import Footer from "./Footer";
-// export const PrivateLayout = () => {
-//   if (!localStorage.getItem("token")) {
-//     console.log("inside protected");
-//     return <Navigate to="/public/login" />;
-//   }
-
-//   return (
-//     <>
-//       <Navbar />
-//       <Outlet />
-//       <Footer/>
-//     </>
-//   );
-// };
